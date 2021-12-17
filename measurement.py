4f933f7566e5e86f5eb6dcfd2d3fca8bc84dbfbf
ihoxofixu <ve.gafurov@gmail.com>
2021-12-17 03:46:53 +0300

    calculaion result with its errors rework, though there are some problems when the value is zero

--- measurement.py
+++ measurement.py
@@ -1,3 +1,18 @@
+def deriviative_two_variables(x, y, func, var_x=True, var_y=True):
+    dev_x = (func(x + x / 100000, y) - func(x, y)) / (x / 100000) if var_x else 0
+    dev_y = (func(x, y + y / 100000) - func(x, y)) / (y / 100000) if var_y else 0
+    return dev_x, dev_y
+
+
+def calc_new_value(x, y, func):
+    var_x = isinstance(x, measurement)
+    var_y = isinstance(y, measurement)
+    x = x if var_x else measurement(x, 0)
+    y = y if var_y else measurement(y, 0)
+    dev_x, dev_y = deriviative_two_variables(x.value, y.value, func, var_x, var_y)
+    return measurement(func(x.value, y.value), x.error * dev_x ** 2 + y.error * dev_y ** 2, square_errors=True)
+
+
 class measurement:
     def __init__(self, value, *errors, square_errors=False):
         self.value = value
@@ -10,68 +25,54 @@ class measurement:
         self.error = res_error
 
     def __add__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value + other, self.error, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(self.value + other.value, self.error + other.error, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Can only add int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(self, other, lambda x, y: x + y)
 
     def __radd__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value + other, self.error, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(self.value + other.value, self.error + other.error, square_errors=True)
-        else:
-            raise TypeError("Can only add to int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        if not isinstance(other, (int, float, measurement)):
+            raise TypeError("Can only add int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(other, self, lambda x, y: x + y)
+
+    def __sub__(self, other):
+        if not isinstance(other, (int, float, measurement)):
+            raise TypeError("Can only substract int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(self, other, lambda x, y: x - y)
 
     def __rsub__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(other - self.value, self.error, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(other.value - self.value, self.error + other.error, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Can only substract from int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(other, self, lambda x, y: x - y)
 
     def __mul__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value * other, self.error * other ** 2, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(self.value * other.value, self.error * other.value ** 2 + other.error * self.error ** 2, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Can only multiply by int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(self, other, lambda x, y: x * y)
 
     def __rmul__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value * other, self.error * other ** 2, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(self.value * other.value, self.error * other.value ** 2 + other.error * self.value ** 2, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Only int, float or measurement type values can be multiplied by, not " + str(type(other)).split("'")[1])
+        return calc_new_value(other, self, lambda x, y: x * y)
 
     def __truediv__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value / other, self.error / other ** 2, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(self.value / other.value, self.error / other.value ** 2 + other.error * (self.value / other.value ** 2) ** 2, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Can only divide by int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(self, other, lambda x, y: x / y)
 
     def __rtruediv__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(other / self.value, self.error / other ** 2, square_errors=True)
-        elif isinstance(other, measurement):
-            return measurement(other.value / self.value, other.error / self.value ** 2 + self.error * (other.value / self.value ** 2) ** 2, square_errors=True)
-        else:
+        if not isinstance(other, (int, float, measurement)):
             raise TypeError("Only int, float or measurement type values can be divided by , not " + str(type(other)).split("'")[1])
+        return calc_new_value(other, self, lambda x, y: x / y)
 
     def __pow__(self, other):
-        if isinstance(other, (int, float)):
-            return measurement(self.value ** other, other ** 2 * self.error * (self.value ** 2) ** (other - 1), square_errors=True)
-        elif isinstance(other, measurement):
-            raise NotReadyYetError("In progress, not ready yet, sorry")
-        else:
+        if isinstance(other, (int, float, measurement)):
             raise TypeError("Can only raise to power of int, float or measurement type values, not " + str(type(other)).split("'")[1])
+        return calc_new_value(self, other, lambda x, y: x ** y)
+
+    def __pow__(self, other):
+        if isinstance(other, (int, float, measurement)):
+            raise TypeError("Only int, float or measurement type values can be raised to power, not " + str(type(other)).split("'")[1])
+        return calc_new_value(other, self, lambda x, y: x ** y)
 
     def __str__(self):
         return str(self.value) + " ± " + str(self.error**0.5)
