2a84ef131db4f70febf4c4d69a69df207fe9fada
ihoxofixu <ve.gafurov@gmail.com>
2022-02-21 18:18:05 +0300

    __init__ rework and plot rework

--- measurement.py
+++ measurement.py
@@ -1,129 +1,125 @@
-import sympy
+import sympy as smp
+import matplotlib.pyplot as plt
+import numpy as np
+import types
 
 
 def calc_new_value(x_var, y_var, func):
     x_var = x_var if isinstance(x_var, measurement) else measurement(x_var, 0)
     y_var = y_var if isinstance(y_var, measurement) else measurement(y_var, 0)
 
-    x = sympy.Symbol("x")
-    y = sympy.Symbol("y")
+    x = smp.Symbol("x")
+    y = smp.Symbol("y")
     tmp_f = func(x, y)
 
-    dev_x = sympy.lambdify((x, y), sympy.diff(tmp_f, x))(x_var.value, y_var.value)
-    dev_y = sympy.lambdify((x, y), sympy.diff(tmp_f, y))(x_var.value, y_var.value)
+    dev_x = smp.lambdify((x, y), smp.diff(tmp_f, x))(x_var.value, y_var.value)
+    dev_y = smp.lambdify((x, y), smp.diff(tmp_f, y))(x_var.value, y_var.value)
     return measurement(func(x_var.value, y_var.value), x_var.error * dev_x ** 2 + y_var.error * dev_y ** 2, square_errors=True)
+
+
+def round_to_3(x):
+    dec_cnt = 0
+    while abs(x) < 100:
+        x *= 10
+        dec_cnt += 1
+    x = round(x)
+    return str(x / 10 ** dec_cnt) + "0" * (x % 10 == 0)
+
+
+def grex_round(val, grex):
+    dec_cnt = 0
+    while abs(val) < 100:
+        val *= 10
+        dec_cnt += 1
+    grex = round(grex * 10 ** dec_cnt) / 10 ** dec_cnt
+    return str(grex)
 
 
 class measurement:
     def __init__(self, value, *errors, square_errors=False):
-        self.value = value
-        res_error = 0
-        for error in errors:
-            if square_errors:
-                res_error += error
-            else:
-                res_error += error ** 2
-        self.error = res_error
+        if not isinstance(value, (int, float, measurement)):
+            raise ValueError("measurement() arguments must be \'measurement\' or numbers, not \'" + str(type(value)).split("'")[1] + "\'")
+        elif isinstance(value, measurement):
+            self.value = value.value
+            self.error = value.error
+        else:
+            self.value = value
+            res_error = 0
+            for error in errors:
+                if not isinstance(error, (int, float, measurement)):
+                    raise ValueError("measurement() arguments must be \'measurement\' or numbers, not \'" + str(type(error)).split("'")[1] + "\'")
+                res_error += error ** (1 + square_errors)
+            self.error = res_error
 
     def __add__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only add int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(self, other, lambda x, y: x + y)
 
     def __radd__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only add int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(other, self, lambda x, y: x + y)
 
     def __sub__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only substract int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(self, other, lambda x, y: x - y)
 
     def __rsub__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only substract from int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(other, self, lambda x, y: x - y)
 
     def __mul__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only multiply by int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(self, other, lambda x, y: x * y)
 
     def __rmul__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Only int, float or measurement type values can be multiplied by, not " + str(type(other)).split("'")[1])
         return calc_new_value(other, self, lambda x, y: x * y)
 
     def __truediv__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only divide by int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(self, other, lambda x, y: x / y)
 
     def __rtruediv__(self, other):
-        if not isinstance(other, (int, float, measurement)):
-            raise TypeError("Only int, float or measurement type values can be divided by , not " + str(type(other)).split("'")[1])
         return calc_new_value(other, self, lambda x, y: x / y)
 
     def __pow__(self, other):
-        if isinstance(other, (int, float, measurement)):
-            raise TypeError("Can only raise to power of int, float or measurement type values, not " + str(type(other)).split("'")[1])
         return calc_new_value(self, other, lambda x, y: x ** y)
 
     def __rpow__(self, other):
-        if isinstance(other, (int, float, measurement)):
-            raise TypeError("Only int, float or measurement type values can be raised to power, not " + str(type(other)).split("'")[1])
         return calc_new_value(other, self, lambda x, y: x ** y)
 
-    def __str__(self):
-        return str(self.value) + " ± " + str(self.error**0.5)
+    def __str__(self, digits=0):
+        if digits != 0:
+            return str(round(value.value, digits)) + " ± " + str(round(value.error**0.5, digits))
+        else:
+            return str(round_to_3(value.value)) + " ± " + str(round_grex(value.value, value.error**0.5))
 
 
-def fit_linear(x_values, y_values, method="MNK"):
-    if len(x_values) != len(y_values):
-        raise SizeError("Incorrect data, can not pair the values, please check the amount of elements")
-    elif len(x_values) < 2:
-        raise SizeError("Not enought data, can not fit the line")
-    n = len(x_values)
+def grex_linear_MNK(k, b, x, y):
+    Dxx = np.var(x)
+    Dyy = np.var(y)
+    grex_k = ((Dyy / Dxx - k ** 2) / (len(x) - 2)) ** 0.5
+    grex_b = grex_k * (np.mean(x ** 2)) ** 0.5
+    return grex_k, grex_b
 
-    if method not in ["MNK", "CHI"]:
-        raise MethodError("Unrecognised method, can only fit using MNK or CHI method")
 
-    x_sum, y_sum, xx_sum, xy_sum = 0, 0, 0, 0
-    w_x_tot, w_y_tot, w_xx_tot, w_xy_tot = 0, 0, 0, 0
-    for i in range(len(x_values)):
-        if not isinstance(x_values[i], (int, float, measurement)):
-            raise TypeError("Can not fit using " + str(type(x_values[i])).split("'")[1] + " type values, only int, float or measurement type")
-        elif isinstance(x_values[i], (int, float)):
-            if method != "CHI":
-                x_values[i] = measurement(x_values[i], 0)
-            else:
-                raise TypeError("Can not use definite values(int or float) in CHI method")
-
-        if not isinstance(y_values[i], (int, float, measurement)):
-            raise TypeError("Can not fit using " + str(type(y_values[i])).split("'")[1] + " type values, only int, float or measurement type")
-        elif isinstance(y_values[i], (int, float)):
-            if method != "CHI":
-                y_values[i] = measurement(y_values[i], 0)
-            else:
-                raise TypeError("Can not use definite values(int or float) in CHI method")
-
-        w_x, w_y, w_xx, w_xy = 1, 1, 1, 1
-        if method == "CHI":
-            w_x = 1 / x_values[i].error
-            w_y = 1 / y_values[i].error
-            w_xx = 1 / (x_values[i] ** 2).error
-            w_xy = 1 / (x_values[i] * y_values[i]).error
-        x_sum += x_values[i].value * w_x
-        y_sum += y_values[i].value * w_y
-        xx_sum += x_values[i].value ** 2 * w_xx
-        xy_sum += x_values[i].value * y_values[i].value * w_xy
-        w_x_tot += w_x
-        w_y_tot += w_y
-        w_xx_tot += w_xx
-        w_xy_tot += w_xy
-
-    x_av, y_av, xx_av, xy_av = x_sum / w_x_tot, y_sum / w_y_tot, xx_sum / w_xx_tot, xy_sum / w_xy_tot
-    k_value = (xy_av - x_av * y_av) / (xx_av - x_av ** 2)
-    b_value = y_av - k_value * x_av
-    return k_value, b_value
+def plot_exprimental(x, y, xerr=0, yerr=0,
+                     xlbl="x", ylbl="y", xmu="", ymu="",
+                     fitline=False):
+    plt.figure(figsize=(15, 6))
+        if isinstance(xerr, types.FunctionType):
+            xerr = xerr(np.array(x))
+        if isinstance(yerr, types.FunctionType):
+            yerr = yerr(np.array(y))
+        plt.errorbar(x, y, xerr=xerr, yerr=yerr,
+                     marker="o", ms=6.5,
+                     linewidth=0, elinewidth=1.8,
+                     mec="black", mfc="black", ecolor="grey",
+                     label="Experimantal " + ylbl + "(" + xlbl + ")")
+    if fitline:
+        k, b = np.polyfit(np.array(x), np.array(y), 1)
+        plt.plot(x, k * x + b, 'b-', label="y = k * x + b", color="red")
+        grex_k, grex_b = grex_linear_MNK(k, b, x, y)
+    plt.xlabel(xlbl+", " * (len(xmu) != 0)+xmu)
+    plt.ylabel(ylbl+", " * (len(ymu) != 0)+ymu)
+    plt.legend()
+    plt.minorticks_on()
+    plt.grid(which='major')
+    plt.grid(which='minor', linestyle=':')
+    plt.show()
+    if fitline:
+        print("k = " + round_to_3(k) + " ± " + grex_round(k, grex_k))
+        print("b = " + round_to_3(b) + " ± " + grex_round(b, grex_b))
